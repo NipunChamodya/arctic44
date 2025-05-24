eeffd30395e02ef2b5c2e51e37e3a54822301696
Wijasinhayalage Karunarathna <nipunchamodya20020627@gmail.com>
2025-05-24 20:21:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function updateCountdown() {
   const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const secs = Math.floor((distance % (1000 * 60)) / 1000);
 
-  if (distance > 0) {
-    document.getElementById("countdown").innerHTML =
-      `${days}d ${hours}h ${mins}m ${secs}s`;
-  } else {
-    clearInterval(timer);
-    window.location.href = "response.html";
-  }
+  // if (distance > 0) {
+  //   document.getElementById("countdown").innerHTML =
+  //     `${days}d ${hours}h ${mins}m ${secs}s`;
+  // } else {
+  //   clearInterval(timer);
+  //   window.location.href = "response.html";
+  // }
 }
 
 let timer = setInterval(updateCountdown, 1000);
