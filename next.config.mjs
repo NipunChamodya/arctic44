2ebf6a9787a3ee04fd30539a13988834b0a5b5c0
Wijasinhayalage Karunarathna <nipunchamodya20020627@gmail.com>
2025-06-24 14:56:00 +0530

    statis page genarated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ✅ This is the key setting
   reactStrictMode: true,
   devIndicators: false,
+  images: {
+    unoptimized: true, // ✅ Required for static export if you're using <Image>
+  },
 };
 
 export default nextConfig;
+
